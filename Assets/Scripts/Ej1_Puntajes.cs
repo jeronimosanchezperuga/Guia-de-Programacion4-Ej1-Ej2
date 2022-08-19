068974e0520fc2d70d043f8dd0c8ffd6a6a6ab6b
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-08-19 11:05:57 -0300

    Initial commit

---
+++ Assets/Scripts/Ej1_Puntajes.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej1_Puntajes : MonoBehaviour
+{
+    [SerializeField] int[] puntajes = new int[10];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("El mayor puntaje es " + ObtenerMayorPuntaje());
+        Debug.Log("El nivel con menor puntaje es " + ObtenerNivelDeMenorPuntaje());
+        Debug.Log("El promedio es " + ObtenerPuntajePromedio());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    int ObtenerMayorPuntaje()
+    {
+        int mayorPuntaje = 0;
+
+        foreach (int puntaje in puntajes)
+        {
+            if (puntaje > mayorPuntaje)
+            {
+                mayorPuntaje = puntaje;
+            }
+        }
+
+        return mayorPuntaje;
+    }
+
+    int ObtenerNivelDeMenorPuntaje()
+    {
+        int indiceNivelMenorPuntaje = 0;
+        int menorPuntaje = puntajes[0];
+
+        for(int i= 1;i<puntajes.Length; i++)
+        {
+            if (puntajes[i] < menorPuntaje)
+            {
+                menorPuntaje = puntajes[i];
+                indiceNivelMenorPuntaje = i;
+            }
+        }
+
+        return indiceNivelMenorPuntaje;
+    }
+
+    float ObtenerPuntajePromedio()
+    {
+        float promedio = 0;
+        float suma = 0;
+        foreach (int num in puntajes)
+        {
+            suma += num;
+        }
+        promedio = suma / puntajes.Length;
+
+        return promedio;
+    }
+
+}
---
+++ Assets/Scripts/Ej2_DiasYMeses.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej2_DiasYMeses : MonoBehaviour
+{
+    [SerializeField] string[] nombresMeses = new string[12];
+    [SerializeField] int[] diasPorCadaMes = new int[12];
+    public string fechaIngresada;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ValidarFormatoFecha(fechaIngresada);
+    }
+
+    bool ValidarFormatoFecha(string fecha)
+    {
+        //La regla es que sea un string de 4 caracters donde los dos primeros 
+        // caracteres sean un número entre 01 y 31 
+        // y los dos últimos un número entre 01 y 12.
+
+        bool fechaValida = false;
+
+        if (fecha.Length == 4)
+        {
+            string dia = "";
+            string mes = "";
+            dia += fecha[0];
+            dia += fecha[1];
+            int diaNumber = int.Parse(dia);
+            mes += fecha[2];
+            mes += fecha[3];
+            int mesNumber = int.Parse(mes);
+            if (diaNumber >0 && diaNumber<=31 && mesNumber >0 && mesNumber<=12)
+            {
+                fechaValida = true;
+            }
+        }
+        else
+        {
+            Debug.Log("El largo de la fecha es incorrecto");
+        }
+
+        return fechaValida;
+    }
+}
